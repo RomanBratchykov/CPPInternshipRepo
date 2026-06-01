6cc2b1684e61c56f6fcae89e974f99eb7d4d8d44
RomanBratchykov <romanbratchykov@gmail.com>
2026-06-01 15:57:48 +0300

    [8] Completed Task 3 tests

--- Task3/Analyzer.h
+++ Task3/Analyzer.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "FileReader.h"
 #include "Counter.h"
 #include <filesystem>
 #include <string>
@@ -8,73 +7,73 @@
 
 namespace ISXCounter
 {
-class Analyzer
-{
-public:
-	// METHODS
-	static Counter AnalyzeFile(const std::filesystem::path& file_path)
+	class Analyzer
 	{
-		Counter result;
-		result.total_files = 1;
-
-		std::ifstream file(file_path);
-		if (!file.is_open())
+	public:
+		// METHODS
+		static Counter AnalyzeFile(const std::filesystem::path& file_path)
 		{
-			return result;
-		}
+			Counter result;
+			result.total_files = 1;
 
-		std::string line;
-		bool in_multiline_comment = false;
-
-		while (std::getline(file, line))
-		{
-			size_t start_pos = line.find_first_not_of(" \t\r\n");
-
-			if (start_pos == std::string::npos)
+			std::ifstream file(file_path);
+			if (!file.is_open())
 			{
+				return result;
+			}
+
+			std::string line;
+			bool in_multiline_comment = false;
+
+			while (std::getline(file, line))
+			{
+				size_t start_pos = line.find_first_not_of(" \t\r\n");
+
+				if (start_pos == std::string::npos)
+				{
+					if (in_multiline_comment)
+					{
+						result.comment_lines++;
+					}
+					else
+					{
+						result.blank_lines++;
+					}
+					continue;
+				}
+
+				std::string trimmed = line.substr(start_pos);
+
 				if (in_multiline_comment)
 				{
 					result.comment_lines++;
-				}
-				else
-				{
-					result.blank_lines++;
-				}
-				continue;
-			}
-
-			std::string trimmed = line.substr(start_pos);
-
-			if (in_multiline_comment)
-			{
-				result.comment_lines++;
-				if (trimmed.find("*/") != std::string::npos)
-				{
-					in_multiline_comment = false;
-				}
-			}
-			else
-			{
-				if (trimmed.substr(0, 2) == "//")
-				{
-					result.comment_lines++;
-				}
-				else if (trimmed.substr(0, 2) == "/*")
-				{
-					result.comment_lines++;
-					if (trimmed.find("*/") == std::string::npos)
+					if (trimmed.find("*/") != std::string::npos)
 					{
-						in_multiline_comment = true;
+						in_multiline_comment = false;
 					}
 				}
 				else
 				{
-					result.code_lines++;
+					if (trimmed.substr(0, 2) == "//")
+					{
+						result.comment_lines++;
+					}
+					else if (trimmed.substr(0, 2) == "/*")
+					{
+						result.comment_lines++;
+						if (trimmed.find("*/") == std::string::npos)
+						{
+							in_multiline_comment = true;
+						}
+					}
+					else
+					{
+						result.code_lines++;
+					}
 				}
 			}
-		}
 
-		return result;
-	}
-};
+			return result;
+		}
+	};
 }
--- Task3/FileReadingTests.cpp
+++ Task3/FileReadingTests.cpp
@@ -1,1 +1,106 @@
+#include <gtest/gtest.h>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include "Analyzer.h"
+#include "FileReader.h"
+#include "FileWriter.h"
+#include "Counter.h"
 
+using namespace ISXCounter;
+
+namespace {
+std::filesystem::path Task3DataDir() {
+    return std::filesystem::path("D:\\CPPInternshipRepo\\Task3Data");
+}
+
+std::filesystem::path Task3File(const char* name) {
+    return Task3DataDir() / name;
+}
+
+std::string ReadFileToString(const std::filesystem::path& path) {
+    std::ifstream input(path);
+    std::string content;
+    std::string line;
+    while (std::getline(input, line)) {
+        content += line;
+        content += "\n";
+    }
+    return content;
+}
+}
+
+TEST(FileReadingTests, AnalyzeEmptyFile) {
+    Counter stats = Analyzer::AnalyzeFile(Task3File("test1.h"));
+    EXPECT_EQ(stats.total_files, 1);
+    EXPECT_EQ(stats.blank_lines, 0);
+    EXPECT_EQ(stats.comment_lines, 0);
+    EXPECT_EQ(stats.code_lines, 0);
+}
+
+TEST(FileReadingTests, AnalyzeCommentOnlyFile) {
+    Counter stats = Analyzer::AnalyzeFile(Task3File("test2.cpp"));
+    EXPECT_EQ(stats.total_files, 1);
+    EXPECT_EQ(stats.blank_lines, 2);
+    EXPECT_EQ(stats.comment_lines, 5);
+    EXPECT_EQ(stats.code_lines, 0);
+}
+
+TEST(FileReadingTests, AnalyzeSimpleSourceFile) {
+    Counter stats = Analyzer::AnalyzeFile(Task3File("test3.cpp"));
+    EXPECT_EQ(stats.total_files, 1);
+    EXPECT_EQ(stats.blank_lines, 1);
+    EXPECT_EQ(stats.comment_lines, 3);
+    EXPECT_EQ(stats.code_lines, 1);
+}
+
+TEST(FileReadingTests, AnalyzeHeaderWithGuards) {
+    Counter stats = Analyzer::AnalyzeFile(Task3File("test3.h"));
+    EXPECT_EQ(stats.total_files, 1);
+    EXPECT_EQ(stats.blank_lines, 6);
+    EXPECT_EQ(stats.comment_lines, 3);
+    EXPECT_EQ(stats.code_lines, 6);
+}
+
+TEST(FileReadingTests, AnalyzeCFile) {
+    Counter stats = Analyzer::AnalyzeFile(Task3File("test4.c"));
+    EXPECT_EQ(stats.total_files, 1);
+    EXPECT_EQ(stats.blank_lines, 0);
+    EXPECT_EQ(stats.comment_lines, 5);
+    EXPECT_EQ(stats.code_lines, 1);
+}
+
+TEST(FileReadingTests, AnalyzeComplexHeader) {
+    Counter stats = Analyzer::AnalyzeFile(Task3File("test5.h"));
+    EXPECT_EQ(stats.total_files, 1);
+    EXPECT_EQ(stats.blank_lines, 4);
+    EXPECT_EQ(stats.comment_lines, 3);
+    EXPECT_EQ(stats.code_lines, 16);
+}
+
+TEST(FileReadingTests, ProcessDirectoryAggregatesAllFiles) {
+    Counter stats = FileReader::ProcessDirectory(Task3DataDir());
+    EXPECT_EQ(stats.total_files, 6);
+    EXPECT_EQ(stats.blank_lines, 13);
+    EXPECT_EQ(stats.comment_lines, 19);
+    EXPECT_EQ(stats.code_lines, 24);
+}
+
+TEST(FileReadingTests, WriteReportToTask3Data) {
+    Counter stats;
+    stats.total_files = 6;
+    stats.blank_lines = 16;
+    stats.comment_lines = 19;
+    stats.code_lines = 24;
+
+    const std::filesystem::path report_path = Task3DataDir() / "analysis_report.txt";
+    ASSERT_TRUE(FileWriter::WriteToFile(report_path.string(), stats, 123));
+
+    const std::string content = ReadFileToString(report_path);
+    EXPECT_NE(content.find("Total processed files: 6"), std::string::npos);
+    EXPECT_NE(content.find("Blank lines:           16"), std::string::npos);
+    EXPECT_NE(content.find("Comment lines:         19"), std::string::npos);
+    EXPECT_NE(content.find("Code lines:            24"), std::string::npos);
+    EXPECT_NE(content.find("Execution time:        123 ms"), std::string::npos);
+}
